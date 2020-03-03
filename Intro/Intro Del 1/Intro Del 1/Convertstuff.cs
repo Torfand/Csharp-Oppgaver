d8b5e84cb16b40b75782bd0a9c7ad9fdb75bd5e7
Torfand <torfin@getacademy.no>
2020-03-03 12:03:14 +0100

    x

---
+++ Intro/Intro Del 1/Intro Del 1/Convertstuff.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntroDel1
+{
+    class Convertstuff
+    {
+        static void convertTest()
+        {
+            //To-String
+            var x = true;
+            string s = "" + x;
+            // Vil gjøre True om til string
+
+
+            double d = 13.37;
+            string s2 = d.ToString("F");
+            // Vil gjøre om D til string
+
+            // Fra float/double/string/bool til int
+            float f = 12.12f;
+            int i = Convert.ToInt32(f);
+            // til Float
+            float f2 = Convert.ToSingle(i);
+            // til double
+            double d2 = Convert.ToDouble(s2);
+
+        }
+    }
+}
---
+++ Intro/Intro Del 1/Intro Del 1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+namespace IntroDel1
+{
+
+    class Program
+    {
+        /*To  MAIN ting i en klasse:
+         * Felt = NETSEN!!! JS GLobal Var
+         * Methods = Function JS
+         * 
+         * --Grunnskjellet til en class
+         * --Var
+         * Debugging
+         * --Tilordning > --Valgsetninger & Datatyper Decklarationbs
+         *      int heltall
+         *      float/double desimal
+         *      string = string
+         *      true/false
+         * --Loops-
+         * --Tables
+         * --Funksjoner / Methods
+         * -- Input Output
+         */
+        static void Main(string[] args)
+        {
+
+
+
+            /*
+             * DataType Deklarasjoner: ---
+             * Kan deklarere fler variabler : fex -- bool b1,b2,b3,b4;
+             */
+            bool b; //Boolean -- True/False
+            int i; // Int -- Heltall
+            float f; // Float -- desimal
+            double d;
+            string s; // String -- Text
+
+            //Tilordninger --
+            b = true; // eller false
+            i = 13;
+            f = 13.37f;
+            d = 15.4;
+            s = "Hei på deg";
+            //Går ann og deklarere og tilordne samtidig
+            bool b2 = false;
+            int i2 = 4;
+            float f2 = 4.5f;
+            double d2 = 1.2;
+            string s2 = "Snakkas!";
+            var b3 = true;
+            /* Kan bruke Var ^
+             */
+
+             
+
+
+
+            //    if(args.Length == 0)
+            //    {
+            //    Console.WriteLine(args.Length + "--Parametere");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Antall Parametere: " + args.Length);
+            //    }
+            //    foreach (var test in args)
+            //    {
+            //        //var parno = i + 1;
+            //        Console.WriteLine("Parameter : " + test);
+            //    }
+
+            //}
+        }
+    }
+}
