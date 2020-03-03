c9ea11baf2a50b151b9bb41d307f5ab32b6c8290
Torfand <torfin@getacademy.no>
2020-03-03 12:02:15 +0100

    Initial commit

---
+++ 1 Hva gjør denne koden/hvaGjørDenne/hvaGjørDenne/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace hvaGjørDenne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var range = 250;
+            ReadInputAndUpdateCounts(range);
+        }
+
+        private static void ReadInputAndUpdateCounts(int range)
+        {
+            var counts = new int[range];
+            string text = "Text her!";
+            var letterTotal = 0;
+            while (!string.IsNullOrWhiteSpace(text))
+            {
+                text = Console.ReadLine();
+                letterTotal = RefreshCounts(counts, text, letterTotal);
+                ShowCountnPrecantage(range, counts, letterTotal);
+                ShowLetterTotal(letterTotal);
+            }
+        }
+
+        private static void ShowLetterTotal(int letterTotal)
+        {
+            Console.WriteLine("Totale Bokstaver / Nummere : " + letterTotal);
+        }
+
+        private static void ShowCountnPrecantage(int range, int[] counts, int letterTotal)
+        {
+            for (var i = 0; i < range; i++)
+            {
+                if (counts[i] > 0)
+                {
+                    var character = (char)i;
+                    var Showprecantage = 100 * (double)counts[i] / letterTotal;
+                    var output = "!-  " + character + "  -!" + " ---- " + counts[i] + "     Prosent anndel for denne bokstaven eller nummer : " + Showprecantage.ToString("F2") + "%";
+                    Console.WriteLine(output);
+                }
+            }
+        }
+
+        private static int RefreshCounts(int[] counts, string text, int letterTotal)
+        {
+            foreach (var character in text.ToUpper())
+            {
+                //casting
+                letterTotal++;
+                counts[(int)character]++;
+            }
+
+            return letterTotal;
+        }
+    }
+}
---
+++ Password Generator/PWGEN/PWGEN/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PWGEN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
