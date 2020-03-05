ec7bf03f472469dcb660bd9365950b905930c23d
Torfand <torfin@getacademy.no>
2020-03-05 15:10:05 +0100

    complete

--- PurePuzzles/PurePuzzles 2-3/PurePuzzle 2-3/PurePuzzle 2-3/Program.cs
+++ PurePuzzles/PurePuzzles 2-3/PurePuzzle 2-3/PurePuzzle 2-3/Program.cs
@@ -6,7 +6,70 @@ namespace PurePuzzle_2_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rows = 5;
+            var collums = 7;
+
+            for (int i = -5; i <= rows; i++)
+            {
+
+                if(i == 0) continue;
+                var hashtoright = (rows - Math.Abs(i)) * 2;
+                for (int j = -7; j <= collums; j++)
+                {
+                    var hashwritten = collums - Math.Abs(j);
+                    if (rows - Math.Abs(i) > collums -  Math.Abs(j))
+                    {
+
+                        WriteSpace();
+                    }
+
+                    else if (hashtoright > hashwritten)
+                    {
+                        WriteHashtag();
+                    }
+
+                    else
+                    {
+                        WriteSpace();
+                    }
+                }
+                NewLine();
+            }
+            //foreach (var tag in tagsperLine)
+            //{
+            //    for (int x = 0; x < tag; x++)
+            //    {
+            //        WriteSpace();
+            //    }
+            //    for (int j = -1; j < tag; j++)
+            //    {
+
+            //        WriteHashtag();
+            //    }
+
+            //    v
+                
+
+            //    NewLine();
+            //}
+        }
+
+
+
+        private static void NewLine()
+        {
+            Console.WriteLine(" ");
+        }
+
+        private static void WriteSpace()
+        {
+            Console.Write(" ");
+        }
+
+        private static void WriteHashtag()
+        {
+            Console.Write("#");
         }
     }
 }
+
