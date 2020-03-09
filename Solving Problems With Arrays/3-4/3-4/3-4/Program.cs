1c9d78b643d398c282a84bbe980c0ca11bccfb08
Torfand <torfin@getacademy.no>
2020-03-09 13:14:15 +0100

    x

--- Solving Problems With Arrays/3-4/3-4/3-4/Program.cs
+++ Solving Problems With Arrays/3-4/3-4/3-4/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading;
 
 namespace _3_4
@@ -8,42 +10,83 @@ namespace _3_4
     {
         static void Main(string[] args)
         {
-            task3();
+            //3-4, 3,5
+            Task3();
 
         }
 
-        private static void task3()
+        private static void Task3()
         {
             Console.WriteLine("Skriv Text Her (!-Store Bokstaver-!: ");
             string output = "";
+            var letters = LettersAndCipher(out var ciphertext);
+
+
+            output = Encrypt(letters, output, ciphertext);
+            Console.WriteLine("Kryptert Text : " + output);
+            Console.WriteLine("Konvertere tilbake ? Y/N  :");
+            Decrypt(output, ciphertext, letters);
+        }
+
+        private static string[] LettersAndCipher(out string[] ciphertext)
+        {
             string[] letters =
-            {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X","Y","Z","Æ","Ø","Å",};
-            string[] ciphertext =
-            {"Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "Å", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Ø", "Æ", "Z", "X", "C", "V", "B", "N", "M",};
+            {
+                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+                "W", "X", "Y", "Z", "Æ", "Ø", "Å",
+            };
+            ciphertext = new[]
+            {
+                "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "Å", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Ø", "Æ",
+                "Z", "X", "C", "V", "B", "N", "M",
+            };
+            return letters;
+        }
+
+        private static void Decrypt(string output, string[] ciphertext, string[] letters)
+        {
+            var currentlyCiphered = output;
+            var answer = Console.ReadLine()?.ToUpper();
+            if (answer != "Y" && answer != "N")
+            {
+                return;
+            }
+
+            if (answer == "Y")
+            {
+                output = "";
+                foreach (var check in currentlyCiphered.Select(converted => converted.ToString()))
+                {
+                    for (int j = 0; j < ciphertext.Length; j++)
+                    {
+                        if (check != ciphertext[j]) continue;
 
 
-            var plaintext = Console.ReadLine();
-            var check = "";
+                        output += letters[j];
+                    }
+                }
+            }
+
+            Console.WriteLine("Dekryptert Text : " + output);
+        }
+
+        private static string Encrypt(string[] letters, string output, string[] ciphertext)
+        {
+            var plaintext = "kake".ToUpper();
 
             foreach (var letter in plaintext)
             {
-
-                check = letter.ToString();
+                var check = letter.ToString();
                 for (int i = 0; i < letters.Length; i++)
                 {
-
                     if (check == letters[i])
                     {
                         output += ciphertext[i];
                     }
-
-
-
-
                 }
-                
             }
-            Console.WriteLine("Text : " + output);
+
+            return output;
         }
     }
 }
---
+++ Solving Problems With Arrays/3-4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercise4and5(args[0] == "e", args[1]);
+        }
+        private static void Exercise4and5(bool encrypt, string plaintext)
+        {
+            var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
+            var code = "QWERTYUIOPÅÆØLKJHGFDSAZXCVBNM";
+            var ciphertext = Encrypt(plaintext,
+                encrypt ? alphabet : code,
+                encrypt ? code : alphabet);
+            Console.WriteLine(ciphertext);
+            //plaintext = Decrypt(ciphertext);
+        }
+
+        private static string Encrypt(string plaintext, string alphabet, string code)
+        {
+            var ciphertext = "";
+            for (int i = 0; i < plaintext.Length; i++)
+            {
+                ciphertext += EncryptChar(plaintext[i], alphabet, code);
+            }
+
+            return ciphertext;
+        }
+
+        private static char EncryptChar(char c, string alphabet, string code)
+        {
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                if (alphabet[i] == char.ToUpper(c))
+                {
+                    return code[i];
+                }
+            }
+            return c;
+        }
+    }
+}
