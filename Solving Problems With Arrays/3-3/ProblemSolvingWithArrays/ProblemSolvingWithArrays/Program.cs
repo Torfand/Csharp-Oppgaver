20c36fdc2ccb8590e7bb1ae28c7e8574c79a9125
Torfand <torfin@getacademy.no>
2020-03-09 10:17:03 +0100

    Complete

---
+++ Solving Problems With Arrays/3-3/ProblemSolvingWithArrays/ProblemSolvingWithArrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ProblemSolvingWithArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8};
+            foreach (var number in numbers) Console.Write(number + " ");
+            Console.WriteLine($"Sortert = {IsSorted(numbers)}");
+            numbers = new[] {1, 2, 3, 5, 7, 8};
+            foreach (var number in numbers) Console.Write(number + " ");
+            Console.WriteLine($"Sortert = {IsSorted(numbers)}");
+        }
+
+        private static bool IsSorted(int[] numbers)
+        {
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] < numbers[i - 1]) return false;
+            }
+
+            return true;
+        }
+
+
+    }
+}
---
+++ Solving Problems With Arrays/3-4/3-4/3-4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+
+namespace _3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            task3();
+
+        }
+
+        private static void task3()
+        {
+            Console.WriteLine("Skriv Text Her (!-Store Bokstaver-!: ");
+            string output = "";
+            string[] letters =
+            {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X","Y","Z","Æ","Ø","Å",};
+            string[] ciphertext =
+            {"Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "Å", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Ø", "Æ", "Z", "X", "C", "V", "B", "N", "M",};
+
+
+            var plaintext = Console.ReadLine();
+            var check = "";
+
+            foreach (var letter in plaintext)
+            {
+
+                check = letter.ToString();
+                for (int i = 0; i < letters.Length; i++)
+                {
+
+                    if (check == letters[i])
+                    {
+                        output += ciphertext[i];
+                    }
+
+
+
+
+                }
+                
+            }
+            Console.WriteLine("Text : " + output);
+        }
+    }
+}
