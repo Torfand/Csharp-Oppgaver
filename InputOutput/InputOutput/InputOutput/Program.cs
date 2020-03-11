da64e05a37d89486abef59d4eb2c58d02859ff2e
Torfand <torfin@getacademy.no>
2020-03-11 08:05:14 +0100

    x

--- InputOutput/InputOutput/InputOutput/Program.cs
+++ InputOutput/InputOutput/InputOutput/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
+using System.Text;
 
 namespace InputOutput
 {
@@ -26,41 +28,66 @@ namespace InputOutput
 
         static void Main(string[] args)
         {
+            //var path = @"C:\Users\Get Academy\Documents\Ny mappe\Start-IT\TCA\C#\Oppgaver\InputOutput\InputOutput\InputOutput\data.txt";
+            ////var fil = File.ReadAllLines(path);
+            ////Console.WriteLine(fil); - 
+            ////Skriver ut innhold i txt fil
+
+            //var List = ListFromFile(path);
+
+            //Lines(List); 
+
+            ////var allLines = File.ReadAllLines(path);
+            ////for (var index = 0; index < allLines.Length; index++)
+            ////{
+            ////    var line = allLines[index];
+            ////    var lNO = (index + 1).ToString().PadLeft(3, '0');
+            ////    Console.WriteLine(lNO + ": " + line);
+            ////}
+            ////Skriver ut alt i fil og legger til linjenr
+
+
+            ////Console.WriteLine("Hello"); 
+            ////var input = Console.ReadLine();
+            ////Console.WriteLine("Hello" + input);
+            encondeTEst();
+        }
+
+        private static void encondeTEst()
+        {
             var path = @"C:\Users\Get Academy\Documents\Ny mappe\Start-IT\TCA\C#\Oppgaver\InputOutput\InputOutput\InputOutput\data.txt";
-            //var fil = File.ReadAllLines(path);
-            //Console.WriteLine(fil); - 
-            //Skriver ut innhold i txt fil
+            var Text = File.ReadAllText(path, Encoding.UTF8);
+            Console.WriteLine(Text);
+        }
 
+        private static void Lines(string[] List)
+        {
+            for (var index = 0; index < List.Length; index++)
+            {
+                var line = List[index];
+                Console.WriteLine(index + "  " + line);
+            }
+        }
 
+        private static string[] ListFromFile(string path)
+        {
+            var List = new List<string>();
             using (var streamReader = File.OpenText(path))
             {
                 string readLine = null;
-                int lineNO = 1;
+
                 do
                 {
-                   readLine = streamReader.ReadLine();
-                   if (readLine != null)
-                   {
-                       Console.WriteLine(lineNO + ": " + readLine);
-                       lineNO++;
-                   }
+                    readLine = streamReader.ReadLine();
+
+                    if (readLine != null)
+                    {
+                        List.Add(readLine);
+                    }
                 } while (readLine != null);
             }
 
-            // var allLines = File.ReadAllLines(path);
-            //for (var index = 0; index < allLines.Length; index++)
-            //{
-            //    var line = allLines[index];
-            //    var lNO = (index + 1).ToString().PadLeft(3,'0');
-            //    Console.WriteLine(lNO + ": " + line );
-            //}
-            //Skriver ut alt i fil og legger til linjenr
-
-
-            //Console.WriteLine("Hello"); 
-            //var input = Console.ReadLine();
-            //Console.WriteLine("Hello" + input);
-
+            return List.ToArray();
         }
     }
 }
---
+++ InputOutput/Oppgave-Ordgåte/Oppgave-Ordgåte/Oppgave-Ordgåte/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Oppgave_Ordgåte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
