b45e6c73da94b281b925f338b71c9db96a32093b
Torfand <57662236+Torfand@users.noreply.github.com>
2020-03-13 11:00:37 +0100

    x

---
+++ ObjectsIHTjs/ObjectsIHTjs/Box.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectsIHTjs
+{
+    class Box
+    {
+        public int Width;
+        public int Height;
+
+        public void Show()
+        {
+            Console.WriteLine("Height " + Height  + " Width " + Width);
+        }
+    }
+}
---
+++ ObjectsIHTjs/ObjectsIHTjs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ObjectsIHTjs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Obj iht JS - anonym
+            //var x =  new {Width =  50, Height = 30};
+            var x = new Box
+            {
+                Height = 50,
+                Width =  60
+                
+            };
+
+            x.Show();
+        }
+    }
+}
