77ca1e0ee6e088ae8a838cf125b81f9d415a210d
Torfand <57662236+Torfand@users.noreply.github.com>
2020-03-19 10:01:15 +0100

    Complete

---
+++ FlaskeSim/FlaskeSim/Bottle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mime;
+using System.Text;
+
+namespace FlaskeSim
+{
+    class Bottle
+    {
+        public int Capacity { get; }
+        public int Content { get; private set; }
+
+        public Bottle(int capacity)
+        {
+            Capacity = capacity;
+
+        }
+
+        public bool Fill(int volume)
+        {
+            if (volume + Content > Capacity) return false;
+            Content += volume;
+            return true;
+
+
+
+        }
+        public bool FillToCap(Bottle otherBottle)
+        {
+            var VolumeNeeded = Capacity - Content;
+            if (otherBottle.Content < VolumeNeeded) return false;
+            Content = Capacity;
+            otherBottle.Remove(VolumeNeeded);
+            return true;
+        }
+
+        private void Remove(in int volumeNeeded)
+        {
+            Content -= volumeNeeded;
+        }
+
+        public int EmptyFlask()
+        {
+            var content = Content;
+            Content = 0;
+            return content;
+        }
+    
+
+        public bool isEmpty()
+        {
+            return Content == 0;
+
+        }
+
+        public void FillToCapFromVoid()
+        {
+            Content = Capacity;
+
+        }
+    }
+
+
+}
--- FlaskeSim/FlaskeSim/Program.cs
+++ FlaskeSim/FlaskeSim/Program.cs
@@ -6,7 +6,113 @@ namespace FlaskeSim
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var bottle1 = new Bottle(5);
+            var bottle2 = new Bottle(3);
+            var wanted = 1;
+            var OperationsNO = 1;
+            while (true)
+            {
+                Console.WriteLine("Operations Currently running : " + OperationsNO);
+                var Operations = new int[OperationsNO];
+
+                while (true)
+                {
+                    doOps(Operations, bottle1, bottle2);
+                    if (bottle1.Content == wanted || bottle2.Content == wanted)
+                    {
+                        Console.WriteLine("Sultion found");
+                        showOps(Operations);
+                        Environment.Exit(0);
+                    }
+
+                    var sucsess = updateOps(Operations);
+                    if (!sucsess) break;
+
+                }
+                OperationsNO++;
+            }
+        }
+
+        private static bool updateOps(int[] operations)
+        {
+            int i;
+            for (i = operations.Length - 1; i >= 0; i--)
+            {
+                if (operations[i] < 8)
+                {
+                    operations[i]++;
+                    break;
+                }
+
+                operations[i] = 0;
+
+            }
+            return i != -1;
+        }
+
+        private static void showOps(int[] operations)
+        {
+            var opText = new string[]
+            {
+
+                "Filling Bottle 1 from fount",
+                "Filling Bottle 2 from fount",
+                "Emptying Bottle 1 in Bottle 2",
+                "Emptying Bottle 2 in Bottle 1",
+                "Filling Flask 2 from Flask 1",
+                "Filling Flask 1 from Flask 2",
+                "Emptying Bottle 1 in endless Void",
+                "Emptying Bottle 2 in endless Void",
+
+            };
+            foreach (var operation in operations)
+            {
+                Console.WriteLine(opText[operation]);
+            }
+        }
+
+
+        private static void doOps(int[] ops, Bottle bottle1, Bottle bottle2)
+        {
+            bottle1.EmptyFlask();
+            bottle2.EmptyFlask();
+            foreach (var operation in ops)
+            {
+                if (operation == 0) bottle1.FillToCapFromVoid();
+                else if (operation == 1) bottle2.FillToCapFromVoid();
+                else if (operation == 2)
+                {
+                    if (bottle1.isEmpty()) break;
+                    var success = bottle2.Fill(bottle1.EmptyFlask());
+                    if (!success) break;
+                }
+                else if (operation == 3)
+                {
+                    if (bottle2.isEmpty()) break;
+                    var success = bottle1.Fill(bottle2.EmptyFlask());
+                    if (!success) break;
+                }
+                else if (operation == 4)
+                {
+                    var success = bottle2.FillToCap(bottle1);
+                    if (!success) break;
+                }
+
+                else if (operation == 5)
+                {
+                    var success = bottle1.FillToCap(bottle2);
+                    if (!success) break;
+                }
+                else if (operation == 6)
+                {
+                    bottle1.EmptyFlask();
+                }
+                else if (operation == 7)
+                {
+                    bottle2.EmptyFlask();
+                }
+            }
         }
     }
 }
